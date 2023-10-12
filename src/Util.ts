0be74ea8160f8affa4382a94bfadec13707a3534
Durzn <dev@durzn.com>
2023-10-12 17:58:04 +0200

    Removed unused classes

--- src/Util.ts
+++ src/Util.ts
@@ -9,14 +9,6 @@ export class SymbolWithRange {
 export class SymbolWithIndex {
     constructor(public readonly symbol: string, public readonly start: number) { }
 }
-export class SymbolWithRangeInDepth {
-    constructor(public readonly symbol: HighlightSymbol, public readonly range: vscode.Range, public depth: number) {
-    }
-}
-export class EntryWithDepth {
-    constructor(public readonly symbol: HighlightEntry, public depth: number) {
-    }
-}
 export class EntryWithRange {
     constructor(public readonly symbol: HighlightEntry, public readonly range: vscode.Range) {
     }
@@ -49,9 +41,5 @@ export class Util {
             }
         }
         return symbol;
-    }
-
-    public static sortByProximityToPosition() {
-
     }
 }
