360b8b97c18b6d786314d854d5e3468b02b292aa
Durzn <dev@durzn.com>
2023-10-15 17:53:12 +0200

    Added more runs to performance test

--- src/test/suite/performanceMeasure.test.ts
+++ src/test/suite/performanceMeasure.test.ts
@@ -11,14 +11,31 @@ suite('Performance Test Suite', () => {
     test('Sample test', () => {
         let editor = vscode.window.activeTextEditor!;
         assert.notStrictEqual(editor, undefined);
+
         let times: number[] = [];
 
-        for (let line = 0; line < editor.document.lineCount; line++) {
-            editor.selection = new vscode.Selection(new vscode.Position(line, 0), new vscode.Position(line, 0));
-            let startTime = Date.now();
-            handleTextSelectionEvent();
-            let endTime = Date.now();
-            times.push(endTime - startTime);
+        for (let i = 0; i < 3; i++) {
+            let timesOfRun: number[] = [];
+
+            for (let line = 0; line < editor.document.lineCount; line++) {
+                editor.selection = new vscode.Selection(new vscode.Position(line, 0), new vscode.Position(line, 0));
+                let startTime = Date.now();
+                handleTextSelectionEvent();
+                let endTime = Date.now();
+                timesOfRun.push(endTime - startTime);
+            }
+
+            let sortedTimes = timesOfRun.sort();
+            let totalTimeOfRun = sortedTimes.reduce((a, b) => a + b);
+            let meanOfRun: number = totalTimeOfRun / sortedTimes.length
+            let varianceOfRun: number = sortedTimes.reduce((a, b) => a + Math.pow(b - meanOfRun, 2)) / sortedTimes.length;
+            let medianOfRun: number = sortedTimes[Math.floor(sortedTimes.length / 2)];
+            let longestDurationOfRun = Math.max(...sortedTimes);
+            let shortestDurationOfRun = Math.min(...sortedTimes);
+
+            times = times.concat(timesOfRun);
+
+            console.log(`Total time of run: ${totalTimeOfRun / 1000}s; Mean of run: ${meanOfRun}ms; Median of run: ${medianOfRun}ms; Variance of run: ${varianceOfRun}ms; Longest duration of run: ${longestDurationOfRun}ms; Shortest duration of run: ${shortestDurationOfRun}ms`);
         }
 
         let sortedTimes = times.sort();
@@ -29,6 +46,6 @@ suite('Performance Test Suite', () => {
         let longestDuration = Math.max(...sortedTimes);
         let shortestDuration = Math.min(...sortedTimes);
 
-        console.log(`Total time: ${totalTime / 1000}s Mean: ${mean}ms Median: ${median}ms Variance: ${variance}ms Longest duration: ${longestDuration}ms Shortest duration: ${shortestDuration}ms`);
+        console.log(`Total time: ${totalTime / 1000}s; Mean: ${mean}ms; Median: ${median}ms; Variance: ${variance}ms; Longest duration: ${longestDuration}ms; Shortest duration: ${shortestDuration}ms`);
     }).timeout(5000000);
 });
