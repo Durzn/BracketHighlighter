63cd73bda0edee56fa7058b58fafaecd8a4003dc
Durzn <dev@durzn.com>
2023-10-16 19:11:50 +0200

    Disabled performance test for now

--- src/test/suite/performanceMeasure.test.ts
+++ src/test/suite/performanceMeasure.test.ts
@@ -1,5 +1,4 @@
 import * as assert from 'assert';
-import { after } from 'mocha';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -11,6 +10,7 @@ suite('Performance Test Suite', () => {
     test('Sample test', () => {
         let editor = vscode.window.activeTextEditor!;
         assert.notStrictEqual(editor, undefined);
+        return;
 
         let times: number[] = [];
 
